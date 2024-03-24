a93b94ace42b6e9c94e38f72b8f5bf569f64d335
bell-duane12 <130375880+bell-duane12@users.noreply.github.com>
2024-03-24 19:23:40 -0300

    main code

---
+++ Code/ml.py
@@ -0,0 +1,63 @@
+"""
+Projeto: Previsao de Geracao em Usinas Fotovoltaicas.
+"""
+
+def data_reading_from_database(data_frame):
+    """
+    Leitura dos dados do banco em disco para a memoria ram.
+    """
+    print("1) Reading data from database into ram ..")
+    pass
+
+def data_cleansing(data_frame):
+    """
+    Remocao de outliers e dados inconsistentes.
+    """
+    print("2) Data cleansing ..")
+    pass
+
+def data_missing_value_treatment(data_frame):
+    """
+    """
+    print("3) Missing value imputation ..")
+    pass
+
+def data_wrangling(data_frame):
+    """
+    """
+    print("4) Data wrangling ..")
+    pass
+
+def feature_normalization(data_frame):
+    """
+    """
+    pass
+
+def exploratory_data_analysis(data_frame):
+    """
+    """
+    pass
+
+def modeling(data_frame):
+    """
+    """
+    pass
+
+def modeling_evaluation(data_frame):
+    """
+    """
+    pass
+
+def main():
+    """
+    Funcao principal.
+    """
+    data_reading_from_database(None)
+    data_cleansing(None)
+    data_missing_value_treatment(None)
+    data_wrangling(None)
+
+
+if __name__ == "__main__":
+    main()
+    
